717f511e2d34390853f0ae884161883f1b7bec1e
Ryan Pavlik <ryan@sensics.com>
2018-03-30 09:57:04 -0500

    Update build.sh to improve defconfig conditions and copy all overlays.
    Switches to using/requiring bash for this script.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash -e
 
 BUILDROOT=buildroot-2017.02
 
@@ -7,7 +7,8 @@ if [ ! -e $BUILDROOT ]; then
     tar xzf $BUILDROOT.tar.gz
 fi
 
-if [ ! -e $BUILDROOT/.config ]; then
+if [ ! -e $BUILDROOT/.config -o gpio-expand/configs/gpioexpand_defconfig -nt $BUILDROOT/.config ]; then
+    echo "Making gpioexpand_defconfig"
     make -C $BUILDROOT BR2_EXTERNAL="$PWD/gpioexpand" gpioexpand_defconfig
 fi
 
@@ -21,7 +22,10 @@ cp $BUILDROOT/output/images/rpi-firmware/bootcode.bin output
 cp $BUILDROOT/output/images/*.dtb output
 mkdir -p output/overlays
 mv output/dwc2-overlay.dtb output/overlays/dwc2.dtbo
-
+for fn in output/*-overlay.dtb; do
+    base=$(basename ${fn})
+    mv $fn output/overlays/${base/dtb/dtbo}
+done
 echo
 echo Build complete. Files are in output folder.
 echo
